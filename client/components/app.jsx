5ece8e00873f12b3f2a5dbd1b9f808af188eb43c
Theta Likins <thetalikins@Thetas-MacBook-Air.local>
2018-04-15 17:35:56 +1200

    working title broken as well

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const App = () => {
   return (
-//   //   {<!-- Primary Page Layout
-//   // –––––––––––––––––––––––––––––––––––––––––––––––––– -->}
-// <div>
-//  <div className="container">
-//     <div className="row">
-//       <div className="one-half column" style="margin-top: 8%" style="margin-left: 15%">
+
+<div>
+  <h1> working title </h1>
+ {/* <div className="container">
+    <div className="row">
+      <div className="one-half column" style="margin-top: 8%" style="margin-left: 15%">
        
-// {/* // {<!-- instructions -->} */}
+
     <div className="heading">
       <div className="title">
         <h3>Processing Emotions</h3>
@@ -19,35 +19,35 @@ const App = () => {
       <div className="introduction">
         <p> Emotions arise and they fall. Select an emotion from the list below that can help you visualize your emotions as they arise within you and dissolve back into space.</p>
       </div>
-    </div>
-
-// { /* <!-- button/selector --> */}
-//     <div className="dropdown">
-//         <button onClick="selectEmotion()" class="dropbtn">Select Emotion</button>
-//         {/* <!-- these need to be react links --> */}
-//         <div id="myDropdown 1" class="dropdown-content">
-//         <a href="#">Anger</a>
-//         <a href="#">Pride</a>
-//         <a href="#">Shame</a>
-//         </div>
-//         <button onClick="selectTime()" class="dropbtn">Select Emotion</button>
-//         <div id="myDropdown 2" class="dropdown-content">
-//         <a href="#">10 Seconds</a>
-//         <a href="#">30 Seconds</a>
-//         <a href="#">60 Seconds</a>
-//         </div>
-//     </div>
-
-// {/* // vector */}
-//     <div className="visualizer">
-//         <img src="images/sil1.jpg"></img>
-//     </div>
+    </div> */}
 
 
-//     </div>
-//   </div>
-// </div>
-// </div>
+    {/* // <div className="dropdown">
+    //     <button onClick="selectEmotion()" class="dropbtn">Select Emotion</button>
+
+    //     <div id="myDropdown 1" class="dropdown-content">
+    //     <a href="#">Anger</a>
+    //     <a href="#">Pride</a>
+    //     <a href="#">Shame</a>
+    //     </div>
+    //     <button onClick="selectTime()" class="dropbtn">Select Emotion</button>
+    //     <div id="myDropdown 2" class="dropdown-content">
+    //     <a href="#">10 Seconds</a>
+    //     <a href="#">30 Seconds</a>
+    //     <a href="#">60 Seconds</a>
+    //     </div>
+    // </div>
+
+
+    // <div className="visualizer">
+    //     <img src="images/sil1.jpg"></img>
+    // </div> */}
+
+
+    {/* </div>
+  </div>
+ </div> */}
+</div>
   )
 }
 
